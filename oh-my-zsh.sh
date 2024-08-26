c2f4102c4bee0782d37269ba3dec2020838b3c2a
Bruno - Looping4 <looping4.contato@gmail.com>
2024-08-25 22:09:27 -0300

    Adicionado arvo para zsh

---
+++ oh-my-zsh.sh
@@ -0,0 +1,7 @@
+# INSTALAÇÃO OH MY ZSH
+
+#instalar o zsh
+sudo apt install zsh zsh-autosuggestions
+
+#instalar o oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
